8cf59496ddc019c8f11af9972e34e525160c35ae
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-03-03 15:54:00 +0600

    task 5 is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,9 @@
 import "./App.css";
 
 function App() {
-function counter(){
-  let count = 0
-
-return ()=> console.log(count++)
-}
-const count1 = counter()
-count1()
-count1()
-const count2 = counter()
-count2()
-count2()
+console.log(foo)
+const foo = "hello"
+console.log(foo)
 
 
   return (
